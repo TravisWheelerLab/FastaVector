5b9f93fa740c6012d771778d49d38838279ed0bd
George Lesica <george@lesica.com>
2021-11-18 17:57:54 -0700

    Rename test to tests

--- tests/testAssert.h
+++ tests/testAssert.h
@@ -10,17 +10,17 @@
 size_t testNum = 0;
 
 void testAssert(bool testSuccess) {
-  if (!testSuccess) {
-    printf("TEST FAIL: Test %zu returned false.\n", testNum);
-  }
-  testNum++;
+    if (!testSuccess) {
+        printf("TEST FAIL: Test %zu returned false.\n", testNum);
+    }
+    testNum++;
 }
 
 void testAssertString(bool testSuccess, char *message) {
-  if (!testSuccess) {
-    printf("TEST FAIL: Test %zu, %s\n", testNum, message);
-  }
-  testNum++;
+    if (!testSuccess) {
+        printf("TEST FAIL: Test %zu, %s\n", testNum, message);
+    }
+    testNum++;
 }
 
 void resetTestNum() { testNum = 0; }
