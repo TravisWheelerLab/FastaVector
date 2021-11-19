5e3f0572770f88798161af417a7e3b5bd7df1ea8
George Lesica <george@lesica.com>
2021-11-18 18:01:28 -0700

    Format tests

--- tool/check-format.sh
+++ tool/check-format.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-C_FILES=$(find src -type f -name '*.c')
-H_FILES=$(find src -type f -name '*.h')
+C_FILES=$(find src tests -type f -name '*.c')
+H_FILES=$(find src tests -type f -name '*.h')
 
 clang-format --dry-run --Werror ${C_FILES} ${H_FILES}
